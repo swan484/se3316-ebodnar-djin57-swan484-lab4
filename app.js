e2c0d1837c0fbd56dbed5434e5366fdb18d28002
swan484 <swan484@uwo.ca>
2022-11-11 16:52:24 -0500

    add db connection file

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Joi = require("joi")
 const cors = require('cors')
 const express = require("express")
-const mongoose = require("mongoose")
 
 const port = process.env.PORT || 3000;
 const app = express();
