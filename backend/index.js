a9829d4b78d0c21bb303ac5ffcf9b42f4df19e56
ebodnar1 <ebodnar@uwo.ca>
2022-11-14 10:28:06 -0500

    Created backend search functionality and frontend for searching (not responsive)

--- backend/index.js
+++ backend/index.js
@@ -10,34 +10,122 @@ app.use(cors());
 const uri = "mongodb+srv://root:root@cluster0.dklnv6c.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(uri)
 
+const DB_NAME = "music"
+const USERS_COLLECTION = "users"
+const GENRES_COLLECTION = "genres"
+const ALBUMS_COLLECTION = "albums"
+const ARTISTS_COLLECTION = "artists"
+const TRACKS_COLLECTION = "tracks"
+
+app.get("/api/upload", (req, res) => {
+    UploadData(DB_NAME, TRACKS_COLLECTION)
+})
+
 app.get("/api/user/:email", (req, res) => {
     console.log(`Called into GET user (email) with ${req.params.email}`);
-    getUser(req.params.email)
+    getOneFrom(DB_NAME, USERS_COLLECTION, {email: req.params.email})
     .then((data) => res.send(data))
     .catch((err) => res.status(404).send(err));
 });
 
-async function getUser(userEmail){
+/*
+    artist_name, track_genres > genre_title, track_title
+*/
+app.get('/api/search/:query', async (req, res) => {
+    console.log(`Called into GET search (query) with ${req.params.query}`)
+    const queries = req.params.query.split(",")
+
+    var results = []
+    var trackMap = {}
+    for(var q of queries){
+        q = q.trim()
+        console.log(q)
+        const compiledQuery = {
+            $or: [
+                {artist_name: {$regex: q}},
+                {track_title: {$regex: q}},
+                {track_genres: {$elemMatch: {genre_title: {$regex: q}}}}
+            ]
+        }
+        await getAllFrom(DB_NAME, TRACKS_COLLECTION, compiledQuery)
+        .then((data) => {
+            data.forEach(d => {
+                if(!(d.track_id in trackMap)){
+                    trackMap[d.track_id] = 0
+                }
+                trackMap[d.track_id]++;
+                d.count = trackMap[d.track_id];
+                if(trackMap[d.track_id] > 1){
+                    updateCount(results, d.track_id, trackMap[d.track_id])
+                }
+                results.push(d)
+            })
+        })
+        .catch((err) => console.log(`Error: ${err}`))
+    }
+    console.log("Got all results")
+    results.sort((a, b) => parseInt(b.count) - parseInt(a.count))
+    res.send(results)
+})
+
+async function getOneFrom(dbName, collectionName, query){
     await client.connect()
     try {
-        const database = client.db("music");
-        const users = database.collection("users");
-        // Query for a movie that has the title 'The Room'
-        const query = { email: userEmail };
-        const options = {
-        // sort matched documents in descending order by rating
-        sort: { },
-        // Include only the `title` and `imdb` fields in the returned document
-        //projection: { _id: 0, title: 1, imdb: 1 },
-        };
-        const user = await users.findOne(query, options);
+        const database = client.db(dbName);
+        const collection = database.collection(collectionName);
+        const result = await collection.findOne(query, {});
         // since this method returns the matched document, not a cursor, print it directly
-        console.log(user);
-        return user;
+        console.log(`Result: ${result}`);
+        return result;
     } 
     finally {
         await client.close();
     }
+}
+
+const updateCount = (list, id, count) => {
+    index = 0;
+    for(var e of list){
+        if(e.track_id === id){
+            e.count = count;
+            list.splice(index, 1)
+            return e;
+        }
+        index++;
+    }
+}
+
+async function getAllFrom(dbName, collectionName, query){
+    await client.connect()
+    const database = client.db(dbName);
+    const collection = database.collection(collectionName);
+
+    const list = []
+    const result = await collection.find(query);
+    await result.forEach((entry) => {
+        list.push(entry)
+    }).then(async () => {
+        await client.close();
+    })
+    return list;
+}
+
+const UploadData = async (dbName, collectionName) => {
+    await client.connect()
+    var count = 0
+    const database = client.db(dbName);
+    const collection = database.collection(collectionName);
+    await collection.find().forEach(async (c) => {
+        console.log(`Count ${++count} for ${c.track_title}`)
+        if(Array.isArray(c.track_genres) || c.track_genres.length === 0) return;
+        let parsedGenres = JSON.parse(c.track_genres.replace(/'/g, '"'))
+
+        await collection.updateOne({track_id: c.track_id},
+            { $set: { track_genres: parsedGenres } }
+        )
+    })
+   
+    console.log("Finished");
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- src/components/search.js
+++ src/components/search.js
@@ -1,9 +1,102 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import './styles/search.css'
+
+const SearchBar = ({setQuery}) => {
+    const [search, setSearch] = useState('');
+
+    useEffect(() => {
+        setQuery(search)
+    }, [search])
+
+    const updateSearch = (s) => {
+        setSearch(s.target.value)
+    }
+
+    return (
+        <div className='form narrow'>
+            <label>Search any combination of artist, genre, and track title</label>
+            <label className="sub">Separate searches by commas</label>
+            <input autoCorrect="false" type='text' onChange={updateSearch}/>
+        </div>
+    )
+}
+
+const ResultsRow = ({track}) => {
+    return (
+        <div></div>
+    )
+}
 
 const Search = () => {
+    const [state, setState] = useState({
+        query: '',
+        buttonEnabled: true,
+        searchResults: []
+    }) 
+
+    const searchData = () => {
+        console.log("Searching data")
+        toggleButtonEnabled(false)
+        fetch(`http://localhost:3001/api/search/${state.query}`)
+            .then((a) => {
+                return a.json()
+            })
+            .then((a) => {
+                setState({
+                    ...state,
+                    searchResults: a
+                })
+                console.log("Finished search")
+            })
+            .catch(() => {
+                toggleButtonEnabled(true)
+            })
+    }
+
+    const updateQuery = (q) => {
+        setState({
+            ...state,
+            query: q
+        })
+    }
+    const toggleButtonEnabled = (val) => {
+        setState({
+            ...state,
+            buttonEnabled: val
+        })
+    }
+
+    const expandResults = (track) => {
+        console.log(track)
+    }
+
     return(
         <div>
             <h1>Search</h1>
+            <SearchBar setQuery={updateQuery}/>
+            <button onClick={() => searchData()} disabled={!state.buttonEnabled}>Search</button>
+            <ul className="search-table" >
+                {state.searchResults.length > 0 && 
+                    <div className="heading-row table-row">
+                        <li>
+                            <p className="title">TRACK TITLE</p>
+                            <p className="artist">ARTIST</p>
+                            <p className="album">ALBUM</p>
+                            <p className="duration">TIME</p>
+                        </li>
+                    </div>
+                }
+                {state.searchResults.map(item => (
+                    <div className="table-row" key={item.track_id} value={item.track_id} onClick={() => expandResults(item)} >
+                        <li>
+                            <p className="title">{item.track_title}</p>
+                            <p className="artist">{item.artist_name}</p>
+                            <p className="album">{item.album_title}</p>
+                            <p className="duration">{item.track_duration}</p>
+                        </li>
+                    </div>
+                ))}
+            </ul>
         </div>
     )
 }
