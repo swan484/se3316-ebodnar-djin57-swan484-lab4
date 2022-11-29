4238385d66f75c3692712b18985ce9b2aa599c93
swan484 <swan484@uwo.ca>
2022-11-28 19:11:52 -0500

    add search bar to admin

--- src/components/manage.js
+++ src/components/manage.js
@@ -1,16 +1,27 @@
 import React, {useEffect, useState, useCallback} from "react";
+import './styles/manage.css'
 
 const Manage = ({loginStatus}) => {
+    const [search, setSearch] = useState('');
+
+    const updateSearch = (s) => {
+        setSearch(s.target.value)
+    }
+
+    // Default page for non-admins
     if(!loginStatus.admin){
         return (
             <div>
                 <h1>You are not an admin</h1>
-                <p>Only administrators can access this page...</p>
+                <p>Please contact your administrator for help</p>
             </div>
         )
     }
     return (
-        <div>
+        <div className="admin-form">
+            <label className="admin-label">Search by user</label>
+            <input className="admin-input" type="text"/>
+            <button className="admin-button">Search</button>
             <h1>Do admin stuff...</h1>
             <p>Grant other users admin privileges, make reviews hidden (in the view playlists area), manage which users are deactivated</p>
             <p>Make sure greater authentication is used for these endpoints (need to check that calling user is an admin)</p>
