85081135d0a8472765999c08e29629196b384d01
Keon Jin <dkjgoalie@gmail.com>
2022-12-01 20:30:47 -0500

    Add react to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React  from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Layout from './components/layout';
 import Home from './components/home';
