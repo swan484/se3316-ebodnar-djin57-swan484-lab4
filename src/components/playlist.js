d43fb08893de0b5117f5d9258b8088d315bd4180
ebodnar1 <ebodnar@uwo.ca>
2022-12-02 10:47:58 -0500

    Small fix with URL route

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import SongList from "./songlist";
 import './styles/playlist.css'
 
-const {BASE_URL} = require('../../middleware/config.js')
+const BASE_URL = "ec2-100-24-157-29.compute-1.amazonaws.com:3000"
 const UNKNOWN = "Unknown"
 const NONE = "None"
 const NO_DESCRIPTION = "No Description"
